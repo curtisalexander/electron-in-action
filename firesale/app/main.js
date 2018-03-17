c8f0073c8697e7d2596de3f9fdaaa9f716f0c493
Curtis Alexander <calex@calex.org>
2018-03-17 10:40:45 -0500

    prepare to pull in a file from user

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
 let mainWindow = null;
 
@@ -11,9 +12,23 @@ app.on('ready', () => {
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
+    getFileFromUser();
   });
 
   mainWindow.on('closed', () => {
       mainWindow = null;
   });
 });
+
+const getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+
+  if (!files) { return; }
+
+  const file = files[0];
+  const content = fs.readFileSync(file).toString();
+
+  console.log(content);
+}
